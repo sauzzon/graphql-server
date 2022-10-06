c5bd34fa91f7a6e280a7a9d39e7065332ff74c50
Saujan Tiwari <saujan.tiwari@gmail.com>
2022-10-06 12:11:00 +0545

    single book and athor with args

--- server.js
+++ server.js
@@ -62,6 +62,14 @@ const RootQueryType = new GraphQLObjectType({
   name: "Query",
   description: "Root query",
   fields: () => ({
+    book: {
+      type: BookType,
+      description: "A Single Book",
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve: (parent, args) => books.find((book) => book.id === args.id),
+    },
     books: {
       type: new GraphQLList(BookType),
       description: "List of all Books",
@@ -71,6 +79,15 @@ const RootQueryType = new GraphQLObjectType({
       type: new GraphQLList(AuthorType),
       description: "List of all Authors",
       resolve: () => authors,
+    },
+    author: {
+      type: AuthorType,
+      description: "A Single Author",
+      args: {
+        id: { type: GraphQLInt },
+      },
+      resolve: (parent, args) =>
+        authors.find((author) => author.id === args.id),
     },
   }),
 });
